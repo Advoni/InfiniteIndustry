45dbd7e743420ed4626295500c62ebc8453bb450
Nicolas B <Nicolas B@192.168.0.220>
2020-04-18 16:32:13 +0200

    bugfixes

--- src/main/java/net/mcreator/infiniteindustry/block/SilverOreBlock.java
+++ src/main/java/net/mcreator/infiniteindustry/block/SilverOreBlock.java
@@ -18,7 +18,6 @@ import net.minecraft.world.biome.Biome;
 import net.minecraft.world.IWorld;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.ItemGroup;
 import net.minecraft.item.Item;
 import net.minecraft.item.BlockItem;
 import net.minecraft.block.material.Material;
@@ -44,8 +43,7 @@ public class SilverOreBlock extends InfiniteIndustryElements.ModElement {
 	@Override
 	public void initElements() {
 		elements.blocks.add(() -> new CustomBlock());
-		elements.items
-				.add(() -> new BlockItem(block, new Item.Properties().group(ItemGroup.BUILDING_BLOCKS)).setRegistryName(block.getRegistryName()));
+		elements.items.add(() -> new BlockItem(block, new Item.Properties().group(null)).setRegistryName(block.getRegistryName()));
 	}
 	public static class CustomBlock extends Block {
 		public CustomBlock() {
