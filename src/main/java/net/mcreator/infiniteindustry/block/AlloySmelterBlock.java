4f33d57bcbc9325f200ffbefff5e044efdc48d43
Nicolas B <Nicolas B@192.168.0.220>
2020-04-19 15:05:17 +0200

    Added: Alloy Smelter Beta

---
+++ src/main/java/net/mcreator/infiniteindustry/block/AlloySmelterBlock.java
@@ -0,0 +1,72 @@
+
+package net.mcreator.infiniteindustry.block;
+
+import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.common.ToolType;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.world.storage.loot.LootContext;
+import net.minecraft.world.IBlockReader;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.BlockRenderLayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.item.BlockItem;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
+
+import net.mcreator.infiniteindustry.itemgroup.InfiniteIndustryItemGroup;
+import net.mcreator.infiniteindustry.InfiniteIndustryElements;
+
+import java.util.List;
+import java.util.Collections;
+
+@InfiniteIndustryElements.ModElement.Tag
+public class AlloySmelterBlock extends InfiniteIndustryElements.ModElement {
+	@ObjectHolder("infiniteindustry:alloysmelter")
+	public static final Block block = null;
+	public AlloySmelterBlock(InfiniteIndustryElements instance) {
+		super(instance, 49);
+	}
+
+	@Override
+	public void initElements() {
+		elements.blocks.add(() -> new CustomBlock());
+		elements.items
+				.add(() -> new BlockItem(block, new Item.Properties().group(InfiniteIndustryItemGroup.tab)).setRegistryName(block.getRegistryName()));
+	}
+	public static class CustomBlock extends Block {
+		public CustomBlock() {
+			super(Block.Properties.create(Material.IRON).sound(SoundType.METAL).hardnessAndResistance(5f, 30f).lightValue(0).harvestLevel(2)
+					.harvestTool(ToolType.PICKAXE));
+			setRegistryName("alloysmelter");
+		}
+
+		@OnlyIn(Dist.CLIENT)
+		@Override
+		public BlockRenderLayer getRenderLayer() {
+			return BlockRenderLayer.CUTOUT;
+		}
+
+		@Override
+		public boolean isNormalCube(BlockState state, IBlockReader worldIn, BlockPos pos) {
+			return false;
+		}
+
+		@Override
+		public boolean propagatesSkylightDown(BlockState state, IBlockReader reader, BlockPos pos) {
+			return true;
+		}
+
+		@Override
+		public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
+			List<ItemStack> dropsOriginal = super.getDrops(state, builder);
+			if (!dropsOriginal.isEmpty())
+				return dropsOriginal;
+			return Collections.singletonList(new ItemStack(this, 1));
+		}
+	}
+}
