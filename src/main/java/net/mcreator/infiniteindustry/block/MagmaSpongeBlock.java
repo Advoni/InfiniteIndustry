c319ad459ceeff899b1c82e773a0f162538a4afb
Adam <adam_kois@web.de>
2020-04-19 17:15:21 +0200

    Added Magma sponge block update

--- src/main/java/net/mcreator/infiniteindustry/block/MagmaSpongeBlock.java
+++ src/main/java/net/mcreator/infiniteindustry/block/MagmaSpongeBlock.java
@@ -4,7 +4,9 @@ package net.mcreator.infiniteindustry.block;
 import net.minecraftforge.registries.ObjectHolder;
 
 import net.minecraft.world.storage.loot.LootContext;
+import net.minecraft.world.World;
 import net.minecraft.world.IWorldReader;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Item;
 import net.minecraft.item.BlockItem;
@@ -13,6 +15,7 @@ import net.minecraft.block.SoundType;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Block;
 
+import net.mcreator.infiniteindustry.procedures.MagmaSpongeNeighbourBlockChangesProcedure;
 import net.mcreator.infiniteindustry.itemgroup.InfiniteIndustryItemGroup;
 import net.mcreator.infiniteindustry.InfiniteIndustryElements;
 
@@ -50,6 +53,41 @@ public class MagmaSpongeBlock extends InfiniteIndustryElements.ModElement {
 			if (!dropsOriginal.isEmpty())
 				return dropsOriginal;
 			return Collections.singletonList(new ItemStack(this, 1));
+		}
+
+		@Override
+		public void onBlockAdded(BlockState state, World world, BlockPos pos, BlockState oldState, boolean moving) {
+			super.onBlockAdded(state, world, pos, oldState, moving);
+			int x = pos.getX();
+			int y = pos.getY();
+			int z = pos.getZ();
+			{
+				java.util.HashMap<String, Object> $_dependencies = new java.util.HashMap<>();
+				$_dependencies.put("x", x);
+				$_dependencies.put("y", y);
+				$_dependencies.put("z", z);
+				$_dependencies.put("world", world);
+				MagmaSpongeNeighbourBlockChangesProcedure.executeProcedure($_dependencies);
+			}
+		}
+
+		@Override
+		public void neighborChanged(BlockState state, World world, BlockPos pos, Block neighborBlock, BlockPos fromPos, boolean moving) {
+			super.neighborChanged(state, world, pos, neighborBlock, fromPos, moving);
+			int x = pos.getX();
+			int y = pos.getY();
+			int z = pos.getZ();
+			if (world.getRedstonePowerFromNeighbors(new BlockPos(x, y, z)) > 0) {
+			} else {
+			}
+			{
+				java.util.HashMap<String, Object> $_dependencies = new java.util.HashMap<>();
+				$_dependencies.put("x", x);
+				$_dependencies.put("y", y);
+				$_dependencies.put("z", z);
+				$_dependencies.put("world", world);
+				MagmaSpongeNeighbourBlockChangesProcedure.executeProcedure($_dependencies);
+			}
 		}
 	}
 }
---
+++ src/main/java/net/mcreator/infiniteindustry/procedures/MagmaSpongeNeighbourBlockChangesProcedure.java
@@ -0,0 +1,96 @@
+package net.mcreator.infiniteindustry.procedures;
+
+import net.minecraft.world.World;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.block.Blocks;
+
+import net.mcreator.infiniteindustry.block.HotMagmaSpongeBlock;
+import net.mcreator.infiniteindustry.InfiniteIndustryElements;
+
+@InfiniteIndustryElements.ModElement.Tag
+public class MagmaSpongeNeighbourBlockChangesProcedure extends InfiniteIndustryElements.ModElement {
+	public MagmaSpongeNeighbourBlockChangesProcedure(InfiniteIndustryElements instance) {
+		super(instance, 49);
+	}
+
+	public static void executeProcedure(java.util.HashMap<String, Object> dependencies) {
+		if (dependencies.get("x") == null) {
+			System.err.println("Failed to load dependency x for procedure MagmaSpongeNeighbourBlockChanges!");
+			return;
+		}
+		if (dependencies.get("y") == null) {
+			System.err.println("Failed to load dependency y for procedure MagmaSpongeNeighbourBlockChanges!");
+			return;
+		}
+		if (dependencies.get("z") == null) {
+			System.err.println("Failed to load dependency z for procedure MagmaSpongeNeighbourBlockChanges!");
+			return;
+		}
+		if (dependencies.get("world") == null) {
+			System.err.println("Failed to load dependency world for procedure MagmaSpongeNeighbourBlockChanges!");
+			return;
+		}
+		int x = (int) dependencies.get("x");
+		int y = (int) dependencies.get("y");
+		int z = (int) dependencies.get("z");
+		World world = (World) dependencies.get("world");
+		boolean foundA = false;
+		boolean foundB = false;
+		if ((((world.getBlockState(new BlockPos((int) (x + 1), (int) y, (int) z))).getBlock() == Blocks.LAVA.getDefaultState().getBlock())
+				|| (((world.getBlockState(new BlockPos((int) (x - 1), (int) y, (int) z))).getBlock() == Blocks.LAVA.getDefaultState().getBlock())
+						|| (((world.getBlockState(new BlockPos((int) x, (int) (y + 1), (int) z))).getBlock() == Blocks.LAVA.getDefaultState()
+								.getBlock())
+								|| (((world.getBlockState(new BlockPos((int) x, (int) (y - 1), (int) z))).getBlock() == Blocks.LAVA.getDefaultState()
+										.getBlock())
+										|| (((world.getBlockState(new BlockPos((int) x, (int) y, (int) (z + 1)))).getBlock() == Blocks.LAVA
+												.getDefaultState().getBlock())
+												|| ((world.getBlockState(new BlockPos((int) x, (int) y, (int) (z - 1)))).getBlock() == Blocks.LAVA
+														.getDefaultState().getBlock()))))))) {
+			world.setBlockState(new BlockPos((int) x, (int) y, (int) z), HotMagmaSpongeBlock.block.getDefaultState(), 3);
+			if (((world.getBlockState(new BlockPos((int) (x + 1), (int) y, (int) z))).getBlock() == Blocks.LAVA.getDefaultState().getBlock())) {
+				foundA = (boolean) (false);
+				foundB = (boolean) (false);
+				world.setBlockState(new BlockPos((int) (x + 1), (int) y, (int) z), Blocks.WHITE_CONCRETE.getDefaultState(), 3);
+				if (((world.getBlockState(new BlockPos((int) (x + 1), (int) y, (int) (z + 1)))).getBlock() == Blocks.LAVA.getDefaultState()
+						.getBlock())) {
+					world.setBlockState(new BlockPos((int) (x + 1), (int) y, (int) (z + 1)), Blocks.WHITE_CONCRETE.getDefaultState(), 3);
+					if (((world.getBlockState(new BlockPos((int) (x + 1), (int) y, (int) (z + 2)))).getBlock() == Blocks.LAVA.getDefaultState()
+							.getBlock())) {
+						world.setBlockState(new BlockPos((int) (x + 1), (int) y, (int) (z + 2)), Blocks.WHITE_CONCRETE.getDefaultState(), 3);
+					}
+					if (((world.getBlockState(new BlockPos((int) (x + 2), (int) y, (int) (z + 1)))).getBlock() == Blocks.LAVA.getDefaultState()
+							.getBlock())) {
+						world.setBlockState(new BlockPos((int) (x + 2), (int) y, (int) (z + 1)), Blocks.WHITE_CONCRETE.getDefaultState(), 3);
+						foundA = (boolean) (true);
+					}
+				}
+				if (((world.getBlockState(new BlockPos((int) (x + 1), (int) y, (int) (z - 1)))).getBlock() == Blocks.LAVA.getDefaultState()
+						.getBlock())) {
+					world.setBlockState(new BlockPos((int) (x + 1), (int) y, (int) (z - 1)), Blocks.WHITE_CONCRETE.getDefaultState(), 3);
+					if (((world.getBlockState(new BlockPos((int) (x + 1), (int) y, (int) (z - 2)))).getBlock() == Blocks.LAVA.getDefaultState()
+							.getBlock())) {
+						world.setBlockState(new BlockPos((int) (x + 1), (int) y, (int) (z - 2)), Blocks.WHITE_CONCRETE.getDefaultState(), 3);
+					}
+					if (((world.getBlockState(new BlockPos((int) (x + 2), (int) y, (int) (z - 1)))).getBlock() == Blocks.LAVA.getDefaultState()
+							.getBlock())) {
+						world.setBlockState(new BlockPos((int) (x + 2), (int) y, (int) (z - 1)), Blocks.WHITE_CONCRETE.getDefaultState(), 3);
+						foundB = (boolean) (true);
+					}
+				}
+				if (((world.getBlockState(new BlockPos((int) (x + 2), (int) y, (int) z))).getBlock() == Blocks.LAVA.getDefaultState().getBlock())) {
+					world.setBlockState(new BlockPos((int) (x + 2), (int) y, (int) z), Blocks.WHITE_CONCRETE.getDefaultState(), 3);
+					if ((((foundA) == (false)) && ((world.getBlockState(new BlockPos((int) (x + 2), (int) y, (int) (z + 1))))
+							.getBlock() == Blocks.LAVA.getDefaultState().getBlock()))) {
+						world.setBlockState(new BlockPos((int) (x + 2), (int) y, (int) (z + 1)), Blocks.WHITE_CONCRETE.getDefaultState(), 3);
+					}
+					if ((((foundB) == (false)) && ((world.getBlockState(new BlockPos((int) (x + 2), (int) y, (int) (z - 1))))
+							.getBlock() == Blocks.LAVA.getDefaultState().getBlock()))) {
+						world.setBlockState(new BlockPos((int) (x + 2), (int) y, (int) (z - 1)), Blocks.WHITE_CONCRETE.getDefaultState(), 3);
+					}
+					foundB = (boolean) (false);
+					foundA = (boolean) (false);
+				}
+			}
+		}
+	}
+}
